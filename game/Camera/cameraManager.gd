e74f635618e178bd2cd0d15df263019630a613d3
Nademtis <mathias.99.madsen@gmail.com>
2025-03-17 21:00:57 +0100

    small tweaks to camera variables and setup

--- game/Camera/cameraManager.gd
+++ game/Camera/cameraManager.gd
@@ -1,9 +1,10 @@
 extends Node
 class_name CameraManager
 
-var follow_player : bool = true
+var look_ahead : bool = true
 
 @export var follow_offset_strength : float = 15
+
 @export var y_multiplier: float = 1.8  # Increase Y offset strength
 
 @onready var follow_pcam: PhantomCamera2D = $FollowPcam
@@ -14,23 +15,23 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	#refactor to using input vector PlayerStatsplayer_input_vector
+	if look_ahead:
+		camera_lookahead()
+	
+func camera_lookahead () -> void:
 	var target_offset : Vector2
-	if follow_player and PlayerStats.player_input_vector != Vector2.ZERO:
+	if PlayerStats.player_input_vector != Vector2.ZERO:
 		# Normalize velocity to maintain consistent lookahead distance
 		target_offset = PlayerStats.player_input_vector * follow_offset_strength
 		
-		#add to y
-		target_offset.y *= y_multiplier
-		
+		target_offset.y *= y_multiplier #add to y, since screen format
 		follow_pcam.set_follow_offset(target_offset)
 	else:
 		# Reset lookahead when player stops moving
-		follow_pcam.set_follow_offset(Vector2.ZERO)
-		
-		
+		pass
+		#follow_pcam.set_follow_offset(Vector2.ZERO)
 	#print(target_offset)  # Debugging
-	
-	
+
 func freeze_axis (axis: Enums.AXIS) -> void:
 	print("should freeze")
 	if axis == Enums.AXIS.Xaxis:
