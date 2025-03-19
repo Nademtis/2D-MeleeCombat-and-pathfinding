9064f876c1d37cf7d234be23dfea6210f5bc3157
Nademtis <mathias.99.madsen@gmail.com>
2025-03-19 16:12:47 +0100

    small tweaks

--- game/Camera/cameraManager.gd
+++ game/Camera/cameraManager.gd
@@ -19,26 +19,20 @@ func _process(delta: float) -> void:
 	if look_ahead:
 		camera_lookahead()
 		
-		
+	#logic below is for managing camera look ahead
 	var current_input_vector : Vector2 = PlayerStats.player_input_vector
 	if last_input_vector != current_input_vector and current_input_vector != Vector2.ZERO:
 		last_input_vector = current_input_vector
 		change_look_ahead_timer.start()
-		print("timer started")
-		
 	
 func camera_lookahead() -> void:
 	var target_offset : Vector2 = look_ahead_direction * follow_offset_strength
-	target_offset.y *= y_multiplier #add to y, since screen format
-	
+	target_offset.y *= y_multiplier #add to y, since screen format - player need to look further down
 	var current_offset = follow_pcam.get_follow_offset()  # Get current camera offset
 	var smooth_offset = current_offset.lerp(target_offset, 0.025)  # Lerp with a smoothing factor
-	
 	follow_pcam.set_follow_offset(smooth_offset)
-	#print(target_offset)  # Debugging
 
 func freeze_axis (axis: Enums.AXIS) -> void:
-	print("should freeze")
 	if axis == Enums.AXIS.Xaxis:
 		follow_pcam.set_lock_axis(1)
 	if axis == Enums.AXIS.Yaxis:
@@ -65,14 +59,6 @@ func get_direction_from_side(side:Vector2) -> Enums.DIRECTION:
 
 
 func _on_change_look_ahead_timer_timeout() -> void:
+	# only change look ahead direction if same input is held for some time
 	if last_input_vector == PlayerStats.player_input_vector:
-		print("should change")
 		look_ahead_direction = last_input_vector
-	else:
-		print("should not change")
-	
-	#print("change from: ", Enums.DIRECTION.keys()[get_direction_from_side(look_ahead_direction)] )
-	
-	#print("change to: ", Enums.DIRECTION.keys()[get_direction_from_side(look_ahead_direction)] )
-	
-	pass # Replace with function body.
