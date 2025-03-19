7cc3644e267a9e492a43e654834852083f50f9ae
Nademtis <mathias.99.madsen@gmail.com>
2025-03-19 15:49:44 +0100

    camera is niiiice

--- game/Camera/cameraManager.gd
+++ game/Camera/cameraManager.gd
@@ -2,34 +2,39 @@ extends Node
 class_name CameraManager
 
 var look_ahead : bool = true
+@onready var change_look_ahead_timer: Timer = $changeLookAheadTimer
+
+var look_ahead_direction : Vector2
+var last_input_vector: Vector2 = Vector2.ZERO  # Stores last movement direction
 
 @export var follow_offset_strength : float = 15
-
-@export var y_multiplier: float = 1.8  # Increase Y offset strength
+@export var y_multiplier: float = 1.5  # Increase Y offset strength
 
 @onready var follow_pcam: PhantomCamera2D = $FollowPcam
-
 func _ready() -> void:
 	Events.connect("camera_freeze_axis", freeze_axis)
 	Events.connect("camera_stop_freeze_axis", stop_freeze_axis)
 
 func _process(delta: float) -> void:
-	#refactor to using input vector PlayerStatsplayer_input_vector
 	if look_ahead:
 		camera_lookahead()
-	
-func camera_lookahead () -> void:
-	var target_offset : Vector2
-	if PlayerStats.player_input_vector != Vector2.ZERO:
-		# Normalize velocity to maintain consistent lookahead distance
-		target_offset = PlayerStats.player_input_vector * follow_offset_strength
 		
-		target_offset.y *= y_multiplier #add to y, since screen format
-		follow_pcam.set_follow_offset(target_offset)
-	else:
-		# Reset lookahead when player stops moving
-		pass
-		#follow_pcam.set_follow_offset(Vector2.ZERO)
+		
+	var current_input_vector : Vector2 = PlayerStats.player_input_vector
+	if last_input_vector != current_input_vector and current_input_vector != Vector2.ZERO:
+		last_input_vector = current_input_vector
+		change_look_ahead_timer.start()
+		print("timer started")
+		
+	
+func camera_lookahead() -> void:
+	var target_offset : Vector2 = look_ahead_direction * follow_offset_strength
+	target_offset.y *= y_multiplier #add to y, since screen format
+	
+	var current_offset = follow_pcam.get_follow_offset()  # Get current camera offset
+	var smooth_offset = current_offset.lerp(target_offset, 0.025)  # Lerp with a smoothing factor
+	
+	follow_pcam.set_follow_offset(smooth_offset)
 	#print(target_offset)  # Debugging
 
 func freeze_axis (axis: Enums.AXIS) -> void:
@@ -44,7 +49,30 @@ func stop_freeze_axis () -> void:
 	follow_pcam.set_lock_axis(0)
 
 
-func _on_follow_pcam_dead_zone_reached(side: Vector2) -> void:
-	#print("side:", side)
-	#print("deadzone reached")
+func get_direction_from_side(side:Vector2) -> Enums.DIRECTION:
+	var direction : Enums.DIRECTION
+	match side:
+		Vector2(1, 0): direction = Enums.DIRECTION.E
+		Vector2(-1, 0): direction = Enums.DIRECTION.W
+		Vector2(0, 1): direction = Enums.DIRECTION.S
+		Vector2(0, -1): direction = Enums.DIRECTION.N
+		Vector2(1, 1): direction = Enums.DIRECTION.NE
+		Vector2(-1, 1): direction = Enums.DIRECTION.NW
+		Vector2(1, -1): direction = Enums.DIRECTION.SE
+		Vector2(-1, -1): direction = Enums.DIRECTION.SW
+	#print(Enums.DIRECTION.keys()[direction])
+	return direction
+
+
+func _on_change_look_ahead_timer_timeout() -> void:
+	if last_input_vector == PlayerStats.player_input_vector:
+		print("should change")
+		look_ahead_direction = last_input_vector
+	else:
+		print("should not change")
+	
+	#print("change from: ", Enums.DIRECTION.keys()[get_direction_from_side(look_ahead_direction)] )
+	
+	#print("change to: ", Enums.DIRECTION.keys()[get_direction_from_side(look_ahead_direction)] )
+	
 	pass # Replace with function body.
--- game/autoloads/Enums.gd
+++ game/autoloads/Enums.gd
@@ -2,3 +2,4 @@ extends Node
 #global Enums
 
 enum AXIS {Xaxis, Yaxis}
+enum DIRECTION {N, S, E, W, NW, NE, SW, SE}
