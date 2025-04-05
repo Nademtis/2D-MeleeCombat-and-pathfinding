5756aaa052b82958a2c79e4acf8b7e6df36ed62a
Nademtis <mathias.99.madsen@gmail.com>
2025-04-05 15:15:57 +0200

    highlight shader implemented

--- game/worldScenes/on_off_object.gd
+++ game/worldScenes/on_off_object.gd
@@ -4,6 +4,8 @@ class_name OnOffObject
 @export var is_on : bool = false
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+@onready var color_rect: ColorRect = $AnimatedSprite2D/ColorRect #highlight shader
+
 @export var optional_lamp : Lamp = null
 #TODO optional lamp 
 
@@ -14,11 +16,18 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		#todo camera shake?
 		is_on = !is_on
 		if is_on:
-			animated_sprite_2d.play("turn_on")
+			turn_on()
 		else:
-			animated_sprite_2d.play("turn_off")
+			turn_off()
 		emit_signal("toggled", is_on)
 		
 		if optional_lamp:
 			optional_lamp.toggle(is_on)
 		
+func turn_on() -> void:
+	animated_sprite_2d.play("turn_on")
+	color_rect.material.set_shader_parameter("Speed", 0.0)
+	
+func turn_off() -> void:
+	animated_sprite_2d.play("turn_off")
+	color_rect.material.set_shader_parameter("Speed", 0.35)
