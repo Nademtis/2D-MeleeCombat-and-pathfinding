68c78b43639a6ec64582213b2a454ace3342cbcf
Nademtis <mathias.99.madsen@gmail.com>
2025-04-08 21:48:56 +0200

    before file cleanup

--- game/worldScenes/levelSwapper/level_swapper.gd
+++ game/worldScenes/levelSwapper/level_swapper.gd
@@ -8,6 +8,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		#print("should swap level")
 		Events.emit_signal("fade_to_black", 1)
+		Events.emit_signal("immobilize_player")
 		var new_level : Level = Level.new(next_level_url, level_name)
 		await get_tree().create_timer(1.5).timeout
 		LevelManager.load_level(new_level)
